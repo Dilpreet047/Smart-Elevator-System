7eff99714a00cf60d7c028f2fedd26a4ccbb6d24
Dilpreet047 <dilpreetluthra5@gmail.com>
2021-11-19 16:22:17 +0530

    Included Machine Learning

--- elevator.py
+++ elevator.py
@@ -1,21 +1,69 @@
-import sys
+import os
 import urllib.request
 from time import sleep
 import random
+import numpy as np
+import joblib
+from dotenv import load_dotenv
+load_dotenv()
 
-myurl = "https://api.thingspeak.com/update?api_key=5NKLBPLHTEJC42RI"
+myurl1 = os.environ.get("URL1")
+myurl2 = os.environ.get("URL2")
+myurl3 = os.environ.get("URL3")
 
-while True:
+
+def randomFunction():
     speed = random.uniform(0 ,3)
-    governor = 0
     level = random.uniform(-1, 1)
     voltage = random.uniform(100, 260)
     temperature = random.randrange(25, 90)
-    emergency = 0
 
+    return [temperature, level, speed, voltage]
+
+
+def checkEmergency(speed, temperature):
+
+    governor = 0
+    emergency = 0
+    
     if speed > 1.8:
-        governor += 1
+        governor = 1
     if temperature > 82:
-        emergency = 1
-    urllib.request.urlopen(myurl + "&field1=" + str(temperature) + "&field2=" + str(level) + "&field3=" + str(speed) + "&field4=" + str(governor) + "&field5=" + str(voltage) + "&field6=" + str(emergency))
+       emergency = 1
+
+    return [governor, emergency]
+
+
+def checkHealth(temperature, speed, voltage):
+    loaded_model = joblib.load('final_model.sav')
+    val = np.array([temperature, speed, voltage])
+    res = loaded_model.predict(val.reshape(1, -1))
+    if (res == 1.0):
+        return "Healthy"
+    
+    return "Unhealthy"
+
+
+
+while True:
+    temperature, level, speed, voltage = randomFunction()
+    governor, emergency = checkEmergency(speed, temperature)
+    print("Condition of elevator 1 : ", checkHealth(float(temperature), speed, voltage))
+
+    urllib.request.urlopen(myurl1 + "&field1=" + str(temperature) + "&field2=" + str(level) + "&field3=" + str(speed) + "&field4=" + str(governor) + "&field5=" + str(voltage) + "&field6=" + str(emergency))
+
+    sleep(1)
+    temperature, level, speed, voltage = randomFunction()
+    governor, emergency = checkEmergency(speed, temperature)
+    print("Condition of elevator 2 : ", checkHealth(float(temperature), speed, voltage))
+
+    urllib.request.urlopen(myurl2 + "&field1=" + str(temperature) + "&field2=" + str(level) + "&field3=" + str(speed) + "&field4=" + str(governor) + "&field5=" + str(voltage) + "&field6=" + str(emergency))
+
+    sleep(1)
+    temperature, level, speed, voltage = randomFunction()
+    governor, emergency = checkEmergency(speed, temperature)
+    print("Condition of elevator 3 : ", checkHealth(float(temperature), speed, voltage))
+
+    urllib.request.urlopen(myurl3 + "&field1=" + str(temperature) + "&field2=" + str(level) + "&field3=" + str(speed) + "&field4=" + str(governor) + "&field5=" + str(voltage) + "&field6=" + str(emergency))
+
     print("Uploaded")
